8ba2b68de4364d69aa0b8d03addad680cd9413f1
Lazarus-10 <ksarim225@gmail.com>
2021-08-09 20:12:44 +0530

    s

---
+++ 35.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+        
+        sort(a.begin(), a.end());
+        int i = 0, j = m-1;
+        long long Max = a[a.size() - 1];
+        while(j<n){
+            Max = min(Max, a[j]-a[i]);
+            i++;
+            j++;
+        }
+        return Max;
+    }   
+};
+
+// { Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
