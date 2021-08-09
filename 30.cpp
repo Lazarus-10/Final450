a5b5a4b9a7614c8ce34a8c24c6377d2d37381042
Lazarus-10 <ksarim225@gmail.com>
2021-08-09 21:23:28 +0530

    s

---
+++ 30.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+// A C++ program to print elements with count more than n/k
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find all elements in array that appear more than n/k times.
+    int countOccurence(int arr[], int n, int k) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<n; i++){
+            mp[arr[i]]++;
+        }
+        int ratio = n/k;
+        int count = 0;
+        for(auto itr = mp.begin(); itr != mp.end(); itr++){
+            if((*itr).second > ratio)
+                count++;
+        }
+        return count;
+    }
+};
+
+
+// { Driver Code Starts.
+int main() {
+    freopen("30.txt", "r", stdin);
+    int t, k;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+
+        int arr[n];
+
+        for (i = 0; i < n; i++) cin >> arr[i];
+        cin >> k;
+        Solution obj;
+        cout << obj.countOccurence(arr, n, k) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
